7a184cf03e8651d9aecc1b0db6dac53291f6a6e1
sungkuk5420 <sungkuk5420@gmail.com>
2019-04-30 11:16:48 +0900

    Changing Color

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const lineRange = document.getElementById("jsRange");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -31,9 +32,19 @@ function stopPainting(event) {
     painting = false;
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+);
